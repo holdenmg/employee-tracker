e4309b02ad25d5bf476434bd7702b3b68b7c146f
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-08-03 17:59:07 -0400

    Added seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -22,8 +22,8 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name DECIMAL NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name  VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT DEFAULT NULL,
     FOREIGN KEY (role_id)
