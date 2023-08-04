097b1585eff92d7437498a38ef1be094dc8ddccc
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-08-03 21:36:59 -0400

    Added more queries

--- db/query.sql
+++ db/query.sql
@@ -5,11 +5,14 @@ SELECT role.id, role.title, department.name AS department, role.salary FROM role
 -- View employees (Show role as title, department as name, salary, and manager as name) --
 SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, employee.manager FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON department.id = role.department_id
 -- Add department (Name) --
-
+IINSERT INTO department (name)
+VALUES(?)
 -- Add role (Name, salary, department) --
-
+INSERT INTO role (title, salary, department_id)
+VALUES(?)
 -- Add employee (employee’s first name, last name, role, and manager) --
-
--- Update employee (select an employee to update and their new role)
-
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES(?)
+-- Update employee role (select an employee to update and their new role)
+UPDATE employee SET role_id = ? WHERE id = ?
 
