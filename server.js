b2b1f09db0080a09b78e290a0e340a8f64e26394
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-08-04 01:57:18 -0400

    Complted addEMployee function. Added comments

--- server.js
+++ server.js
@@ -22,71 +22,89 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the tracker_db database.`)
 );
-// GLOBAL ARRAY VARIABLES FOR PROMPT CHOICES
 
 
 
 
 
-  //function roleQuestion() {
-   // db.query('SELECT title FROM role', (err, data) => {
-      //if (err) {
-     //  console.log(err);
-    ////  } else {
-     //   var roles = data;
-     //   roleTitles = roles.map(role => role.title);
-     //   console.log(roleTitles);
-    //  }
-    //  return roleTitles;
-   // });
-   // return roleTitles;
-//  }
 
-  function addEmployee() {
-    db.query('SELECT CONCAT (m.first_name," ", m.last_name) AS manager FROM employee LEFT JOIN employee m ON employee.manager_id = m.id', (err, data) => {
+
+// adds employee based on responses to inquirer promts //
+function addEmployee() {
+  //get managers names to feed into inquirer prompt
+  db.query('SELECT CONCAT (m.first_name," ", m.last_name) AS manager FROM employee LEFT JOIN employee m ON employee.manager_id = m.id', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      var managers = data;
+      var managerNames = managers.filter(manager => manager.manager !== null).map(manager => manager.manager);
+    }
+    //get role titles to feed into inquirer prompt
+    db.query('SELECT title FROM role', (err, data) => {
       if (err) {
         console.log(err);
       } else {
-        var managers = data;
-         var managerNames = managers.filter(manager => manager.manager !== null).map(manager => manager.manager);
+        var roles = data;
+        var roleTitles = roles.map(role => role.title);
       }
-      db.query('SELECT title FROM role', (err, data) => {
-        if (err) {
-          console.log(err);
-        } else {
-          var roles = data;
-           var roleTitles = roles.map(role => role.title);
-        }
-        inquirer.prompt([{
-          type: 'input',
-          message: 'Enter employee first name',
-          name: 'firstName',
-        },
-        { 
-          type: 'input',
-          message: 'Enter employee last name',
-          name: 'firstName',},
-        {
-          type: 'input',
-          message: 'Enter employee first name',
-          name: 'firstName',
-        },
-        {
-          type: 'list',
-          message: 'Enter employee role',
-          name: 'role',
-          choices: roleTitles,
-        },
-        {
-          type: 'list',
-          message: 'Enter employee manager',
-          name: 'manager',
-          choices: managerNames,
-        }
-        ])
-      
+      //questions
+      inquirer.prompt([{
+        type: 'input',
+        message: 'Enter employee first name',
+        name: 'firstName',
+      },
+      {
+        type: 'input',
+        message: 'Enter employee last name',
+        name: 'lastName'
+      },
+      {
+        type: 'list',
+        message: 'Select employee role',
+        name: 'role',
+        choices: roleTitles,
+      },
+      {
+        type: 'list',
+        message: 'Select employee manager',
+        name: 'manager',
+        choices: managerNames,
+      }
+      ]).then((answers) => {
+        //get role_id for corresponding title from answers
+        db.query(`SELECT role.id FROM role WHERE role.title = ?;`, answers.role, (err, data) => {
+          if (err) {
+            console.log(err);
+          } else {
+            var roleID = data.map(role => role.id);
+            console.log(roleID);
+          }
+          //get employee id for corresponding manager from answers
+          db.query(`SELECT employee.id FROM employee WHERE CONCAT(employee.first_name," ",employee.last_name) = ?;`, answers.manager, (err, data) => {
+            if (err) {
+              console.log(err);
+            } else {
+              var managerID = data.map(manager => manager.id);
+              console.log(managerID);
+            }
+
+            //add employee to table
+            db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES(?, ?, ?, ?);`, [answers.firstName, answers.lastName, roleID, managerID], (err, data) => {
+              if (err) {
+                console.log(err);
+              } else {
+
+                console.log('Employee Added!');
+                init();
+              }
+            })
+
+          })
+        })
+      })
+    })
   })
-})}
+};
 
 
 
@@ -131,11 +149,6 @@ function viewEmployee() {
     init()
   });
 }
-
-//function addEmployee() {
-
-
-
 
 
 
