421b971851c0d25d3fee3824f5204349510cd450
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-08-03 21:05:35 -0400

    Wrote queries for displaying various tables

--- db/query.sql
+++ db/query.sql
@@ -1,1 +1,15 @@
+-- View Departments --
+SELECT * FROM department
+-- View Roles --
+SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department ON department.id = role.department_id  
+-- View employees (Show role as title, department as name, salary, and manager as name) --
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, employee.manager FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON department.id = role.department_id
+-- Add department (Name) --
+
+-- Add role (Name, salary, department) --
+
+-- Add employee (employee’s first name, last name, role, and manager) --
+
+-- Update employee (select an employee to update and their new role)
+
 
--- server.js
+++ server.js
@@ -22,3 +22,26 @@ const db = mysql.createConnection(
   console.log(`Connected to the tracker_db database.`)
 );
 
+//db.query('SHOW TABLE department')
+db.query('SELECT * FROM department', (err, results) => {
+  if (err) {
+    console.log(err);
+  }
+  console.table(results);
+});
+
+
+db.query('SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department ON department.id = role.department_id  ', (err, results) => {
+  if (err) {
+    console.log(err);
+  }
+  console.table(results);
+});
+
+
+db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department, role.salary, CONCAT (m.first_name," ", m.last_name) AS manager FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON department.id = role.department_id LEFT JOIN employee m ON employee.manager_id = m.id ', (err, results) => {
+  if (err) {
+    console.log(err);
+  }
+  console.table(results);
+});
